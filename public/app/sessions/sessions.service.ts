ee5a6072f7932b91e1be8fe394e55140063d8d12
Joe Eames <joeeames@gmail.com>
2018-04-14 22:51:43 -0600

    fully migrated sessions service to ng2 service

--- public/app/sessions/sessions.service.ts
+++ public/app/sessions/sessions.service.ts
@@ -11,4 +11,28 @@ export class Sessions {
     return this.http.get('/api/sessions/user/' + userId)
       .toPromise();
   }
+
+  getAllSessions() {
+    return this.http.get('/api/sessions').toPromise();
+  }
+  
+  createNewSession(newSession) {
+    return this.http.post('/api/sessions', newSession).toPromise();
+  }
+  
+  getNextUnreviewedSession(userId) {
+    return this.http.get(`/api/users/${userId}/randomUnreviewedSession`).toPromise();
+  }
+  
+  addReviewedSession(userId, sessionId) {
+    return this.http.post('/api/users/' + userId + '/reviewSession/' + sessionId, {}).toPromise();
+  }
+  
+  incrementVote(sessionId) {
+    return this.http.put('/api/sessions/' + sessionId + '/incrementVote/', {}).toPromise();
+  }
+  
+  getUnreviewedCount(userId) {
+    return this.http.get('/api/users/' + userId + '/unreviewedSessionCount').toPromise();
+  }
 }
