2181ade64e039a93ff6a59af1ba9ab7f9d0e9d41
Joe Eames <joeeames@gmail.com>
2018-04-14 22:53:46 -0600

    removed old sessions service

--- public/sessions/unreviewedSessionCount.ts
+++ public/sessions/unreviewedSessionCount.ts
@@ -1,15 +1,14 @@
 angular.module('app').service('unreviewedSessionCount', class UnreviewedSessionCount {
   value: number;
-  sessions: any;
   currentIdentity: any;
   
-  constructor(private sessions_v2, currentIdentity) {
+  constructor(private sessions, currentIdentity) {
     this.value = 0;
     this.currentIdentity = currentIdentity;
   }
   
   updateUnreviewedSessionCount() {
-    this.sessions_v2.getUnreviewedCount(this.currentIdentity.currentUser.id)
+    this.sessions.getUnreviewedCount(this.currentIdentity.currentUser.id)
         .then(response => {
       this.value = response.count;
     })
